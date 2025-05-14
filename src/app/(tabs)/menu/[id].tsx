adbf2e99841b8f0036d7be1364ea8335160f5ca0
josephMasu <josephmuhindo089@gmail.com>
2025-05-14 18:58:57 +0300

    managing selected size with state

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,10 +1,12 @@
 import { Stack, useLocalSearchParams } from "expo-router";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
 import products from "@/assets/data/products";
 import { defaultPizzaImage } from "@/src/components/ProductListItem";
+import { useState } from "react";
 const ProductDetailsScreen = () =>{
     const sizes = ['S', 'M', 'L', 'XL'];
     const {id} = useLocalSearchParams();
+    const [selectedSize, setSelectedSize] = useState('M');
     const product = products.find(p => p.id.toString() === id);
     if(!product){
         return(
@@ -23,12 +25,14 @@ const ProductDetailsScreen = () =>{
             <Text>Select size:</Text>
             <View style={styles.sizes}>
                 {sizes.map(size => (
-                    <View style = {styles.size} key={size}>
-                        <Text style={styles.sizeText}>{size}</Text>                    
-                    </View>
+                    <Pressable 
+                        onPress={() => setSelectedSize(size)}
+                        style = {[styles.size, { backgroundColor: selectedSize === size ? 'gainsboro':'white'}]} key={size}>
+                        <Text style={[styles.sizeText, { color: selectedSize === size ? 'black':'gray'}]}>{size}</Text>                    
+                    </Pressable>
                 ))}
             </View>
-            <Text style={ styles.title}>${product.price}</Text>
+            <Text style={ styles.price}>${product.price}</Text>
         </View>
     );
 };
