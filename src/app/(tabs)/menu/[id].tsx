99f4861afe42735e925857909905e8ef95b66234
josephMasu <josephmuhindo089@gmail.com>
2025-05-14 18:32:40 +0300

    select size component

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -3,6 +3,7 @@ import { View, Text, Image, StyleSheet } from "react-native";
 import products from "@/assets/data/products";
 import { defaultPizzaImage } from "@/src/components/ProductListItem";
 const ProductDetailsScreen = () =>{
+    const sizes = ['S', 'M', 'L', 'XL'];
     const {id} = useLocalSearchParams();
     const product = products.find(p => p.id.toString() === id);
     if(!product){
@@ -19,6 +20,14 @@ const ProductDetailsScreen = () =>{
                 source = {{uri:product.image || defaultPizzaImage}} 
                 style = {styles.image}>
             </Image>
+            <Text>Select size:</Text>
+            <View style={styles.sizes}>
+                {sizes.map(size => (
+                    <View style = {styles.size} key={size}>
+                        <Text style={styles.sizeText}>{size}</Text>                    
+                    </View>
+                ))}
+            </View>
             <Text style={ styles.title}>${product.price}</Text>
         </View>
     );
@@ -40,7 +49,24 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginVertical: 10,
     },
-    title: {},
+    size: {
+        backgroundColor:'gainsboro',
+        width: 50,
+        aspectRatio:1,
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    sizes: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginVertical: 2,
+    },
+    sizeText: {
+        fontSize: 20,
+        fontWeight: '500',
+        marginVertical:10,
+    },
 });
 
 
