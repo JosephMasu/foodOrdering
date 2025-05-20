2dc85af7b8d3a41fa56f7991c59fe9edfd52caf2
josephMasu <josephmuhindo089@gmail.com>
2025-05-21 02:35:59 +0300

    new orders subscription

--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -5,25 +5,12 @@ import { ActivityIndicator } from "react-native";
 import { supabase } from "@/src/lib/supabase";
 import { useEffect } from "react";
 import { useQueryClient } from "@tanstack/react-query";
+import { useInsertOrderSubscription } from "@/src/api/orders/subscrption";
 export default function OrdersXcreen(){
     const {data: orders, isLoading, error} = useAdminOrderList({archived: false});
     const queryClient = useQueryClient();
+    useInsertOrderSubscription();
 
-    useEffect(() => {
-        const ordersSubscription = supabase
-          .channel('custom-insert-channel')
-          .on(
-            'postgres_changes',
-            { event: 'INSERT', schema: 'public', table: 'orders' },
-            (payload) => {
-              queryClient.invalidateQueries({queryKey:['orders']});
-            }
-          )
-          .subscribe();
-        return () => {
-            ordersSubscription.unsubscribe();
-        };
-      }, []);  
     if(isLoading) return <ActivityIndicator/>
     if(error) return <Text>Error: {error.message}</Text>      
     return(
