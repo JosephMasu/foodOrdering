d7bed18226ed457300e3b2b944fd5f05d3cda615
josephMasu <josephmuhindo089@gmail.com>
2025-05-21 02:32:27 +0300

    realtime ts for oerder

--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -1,11 +1,31 @@
 import { Text, FlatList  } from "react-native";
-import orders from "@/assets/data/orders";
 import OrderItemListItem from "@/src/components/OrderListItem";
 import { useAdminOrderList } from "@/src/api/orders";
+import { ActivityIndicator } from "react-native";
+import { supabase } from "@/src/lib/supabase";
+import { useEffect } from "react";
+import { useQueryClient } from "@tanstack/react-query";
 export default function OrdersXcreen(){
     const {data: orders, isLoading, error} = useAdminOrderList({archived: false});
-    if(isLoading) return <Text>Loading...</Text>
-    if(error) return <Text>Error: {error.message}</Text>
+    const queryClient = useQueryClient();
+
+    useEffect(() => {
+        const ordersSubscription = supabase
+          .channel('custom-insert-channel')
+          .on(
+            'postgres_changes',
+            { event: 'INSERT', schema: 'public', table: 'orders' },
+            (payload) => {
+              queryClient.invalidateQueries({queryKey:['orders']});
+            }
+          )
+          .subscribe();
+        return () => {
+            ordersSubscription.unsubscribe();
+        };
+      }, []);  
+    if(isLoading) return <ActivityIndicator/>
+    if(error) return <Text>Error: {error.message}</Text>      
     return(
         <FlatList 
             data={orders} 
