8fbbde6177b6a8422f9248b7272654934dcb0ba6
josephMasu <josephmuhindo089@gmail.com>
2025-05-27 00:22:45 +0300

    creating stripe customer

--- supabase/functions/_utils/stripe.ts
+++ supabase/functions/_utils/stripe.ts
@@ -1,6 +1,5 @@
-// esm.sh is used to compile stripe-node to be compatible with ES modules.
 import Stripe from 'https://esm.sh/stripe@13.10.0?target=deno&deno-std=0.132.0&no-check';
 
-export const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY') ?? '', {
-  httpClient: Stripe.createFetchHttpClient(),
-});
+export const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY')!, {
+  apiVersion: '2023-08-16',
+}) as any;
--- supabase/functions/payment-sheet/index.ts
+++ supabase/functions/payment-sheet/index.ts
@@ -1,5 +1,6 @@
 import Stripe from 'https://esm.sh/stripe@18.1.1?target=deno&deno-std=0.132.0&no-check';
 import type { Stripe as StripeType } from "https://esm.sh/stripe@18.1.1";
+import createOrRetrieveProfile from '../_utils/supabase.ts';
 
 const secretKey = Deno.env.get('STRIPE_SECRET_KEY');
 if (!secretKey) throw new Error("Missing STRIPE_SECRET_KEY");
@@ -8,21 +9,30 @@ export const stripe = new Stripe(secretKey, {
   httpClient: Stripe.createFetchHttpClient(),
 }) as StripeType;
 
-Deno.serve(async (req) => {
+Deno.serve(async (req: Request) => {
   console.log("STRIPE_SECRET_KEY loaded:", !!secretKey);
 
   try {
     const { amount } = await req.json();
+    const customer = await createOrRetrieveProfile(req);
+
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      { customer: customer },
+      { apiVersion: "2020-08-27" }
+    );
 
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: 888,
+      amount: amount,
       currency: "usd",
+      customer: customer,
+      // ephemeralKey: ephemeralKey.secret,
     });
 
-    return new Response(
-      JSON.stringify({
+    return new Response(JSON.stringify({
         paymentIntent: paymentIntent.client_secret,
         publishableKey: Deno.env.get('EXPO_NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY'),
+        ephemeralKey: ephemeralKey.secret,
+        customer: customer,
       }),
       { headers: { "Content-Type": "application/json" } }
     );
